d28c59f1d65349ef296340aaf7c29b212c976ec2
Alena Gilevskaya <alena@gilevskaya.com>
2018-05-09 21:12:44 -0700

    add src

---
+++ packages/react-scripts/template/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+class HomePage extends React.Component {
+  public render() {
+    return (
+      <div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
