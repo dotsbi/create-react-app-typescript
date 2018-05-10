c90d4a763b67b2cf11b1298c2905d67326d07b77
Alena Gilevskaya <alena@gilevskaya.com>
2018-05-09 20:44:13 -0700

    add types

--- packages/react-scripts/template/src/client/components/Link/Link.tsx
+++ packages/react-scripts/template/src/client/components/Link/Link.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import history from '../../history';
 
-const Link = props => {
-  const onClick = e => {
+const Link = (props: any) => {
+  const onClick = (e: MouseEvent) => {
     const aNewTab = e.metaKey || e.ctrlKey;
     const anExternalLink = props.href.startsWith('http');
 
